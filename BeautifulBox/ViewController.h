865c653e03f2defd2d33844a08dfdb099dbf26cf
Miles-Matheson <liyida188@163.com>
2017-02-13 11:32:21 +0800

    Initial Commit

---
+++ BeautifulBox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BeautifulBox
+//
+//  Created by Miles on 17/2/13.
+//  Copyright © 2017年 Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BeautifulBox/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BeautifulBox
+//
+//  Created by Miles on 17/2/13.
+//  Copyright © 2017年 Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
